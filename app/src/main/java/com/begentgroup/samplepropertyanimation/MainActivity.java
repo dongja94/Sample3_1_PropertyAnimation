e52a4d0cb9f6ba18dccadeda31a2f140387152cf
dongja94 <dongja94@gmail.com>
2016-04-22 13:50:28 +0900

    Animator

--- app/src/main/java/com/begentgroup/samplepropertyanimation/MainActivity.java
+++ app/src/main/java/com/begentgroup/samplepropertyanimation/MainActivity.java
@@ -9,16 +9,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    TextSwitcher textSwitcher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.textView);
+        textSwitcher = (TextSwitcher)findViewById(R.id.textSwitcher);
 
         Button btn = (Button)findViewById(R.id.btn_start);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -68,5 +72,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btn = (Button)findViewById(R.id.btn_change);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv = (TextView)textSwitcher.getNextView();
+                tv.setText("" + count++);
+                textSwitcher.showNext();
+            }
+        });
     }
+
+    int count = 0;
 }
